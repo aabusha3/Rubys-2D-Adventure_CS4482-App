ab368aaae7e0b2f08a14f914b3a41450c70a4e30
aabusha3 <aabusha3@uwo.ca>
2023-09-12 16:57:37 -0400

    Up to "tutorial: Decorate the world" done

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RubyController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
+    public float speed = 1.0f;
     void Update()
     {
-        
+        Vector2 position = transform.position;
+        position.x += speed * Input.GetAxis("Horizontal") * Time.deltaTime; 
+        position.y += speed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position = position;
     }
 }
