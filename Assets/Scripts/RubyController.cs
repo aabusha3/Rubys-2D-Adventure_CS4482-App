271309a2d62e8f389024e177308d43a151155e69
aabusha3 <aabusha3@uwo.ca>
2023-09-13 17:10:31 -0400

    Up to "tutorial: World Interactions - Damage Zones and Enemies" done

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    Rigidbody2D rigidbody2d;
-    float horizontal;
-    float vertical;
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    int currentHealth;
+    public float timeInvincible = 2.0f;
+
     public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
 
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-
         currentHealth = maxHealth;
     }
 
@@ -24,6 +29,12 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0) isInvincible = false;
+        }
     }
 
     void FixedUpdate()
@@ -36,6 +47,15 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible) return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
